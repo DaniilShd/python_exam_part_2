2ac258025070423c9a04d94d94539bb0d913281d
DaniilShd <ds_const@mail.ru>
2024-10-21 22:02:08 +0300

    add exercise1

--- exercise1/main.py
+++ exercise1/main.py
@@ -1,4 +1,18 @@
 import logging
+import cowsay
 
+#Настройка логирования
 logging.basicConfig(level=logging.DEBUG, filename='app.log', filemode='w', encoding='utf-8', format='%(levelname)s: %(message)s')
 
+def main():
+    try:
+        command = input("Введите сообщение")
+    except ValueError as err:
+        logging.error(f"при вводе данных возникла ошибка {err}")
+    except TypeError as err:
+        logging.error(f"при вводе данных возникла ошибка {err}")
+    cowsay.cow(command)
+    logging.info("Программа успешно выполнена")
+
+if __name__ == "__main__":
+    main()
