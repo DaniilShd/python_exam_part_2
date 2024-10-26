839041466d864f737ae53718e0dd058a12483e4a
DaniilShd <ds_const@mail.ru>
2024-10-26 09:44:31 +0300

    Add exercise2

--- exercise2/main.py
+++ exercise2/main.py
@@ -1,23 +1,18 @@
-import logging
 import requests
+import logging
 import json
 
-#Настройка логирования
-logging.basicConfig(level=logging.DEBUG, filename='app.log', filemode='w', encoding='utf-8', format='%(levelname)s: %(message)s')
-
-
-def main():
-    url = "https://dummyjson.com/test"
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
+def fetch_data():
+    url = "https://httpbin.org/get"
     try:
         response = requests.get(url)
-        #Проверка статуса ответа
-        response.raise_for_status() 
+        response.raise_for_status() # Проверка статуса ответа
         data = response.json()
-        print(json.dumps(data, indent=4))
+        print(json.dumps(data, indent=4)) # Красивый вывод JSON
         logging.info("Данные успешно получены")
     except requests.exceptions.RequestException as e:
-        logging.error(e)
         logging.error(f"Ошибка при выполнении запроса: {e}")
-
 if __name__ == "__main__":
-    main()
+    fetch_data()
